119dc870fc8efe7adb0c2199e9c4afff9bb3d3ae
Thai <75675012+thai-oddle@users.noreply.github.com>
2023-04-21 17:07:57 +0000

    Update generate-fake-data.js and Timeline.scss

--- demo/app/generate-fake-data.js
+++ demo/app/generate-fake-data.js
@@ -2,7 +2,7 @@ import faker from 'faker'
 import randomColor from 'randomcolor'
 import moment from 'moment'
 
-export default function (groupCount = 30, itemCount = 1000, daysInPast = 30) {
+export default function (groupCount = 100, itemCount = 1000, daysInPast = 30) {
   let randomSeed = Math.floor(Math.random() * 1000)
   let groups = []
   for (let i = 0; i < groupCount; i++) {
